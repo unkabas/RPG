e4ce5b50319f52a25d0ff318326b2396c5cd2f54
unkabas <undercannabis10@gmail.com>
2025-05-18 04:41:46 -0700

    Update Enemy.cs

--- Enemy.cs
+++ Enemy.cs
@@ -1,40 +1,41 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
- 
+
 public class Enemy : MonoBehaviour
 {
     public int health;
     public float speed = 1f;
+    public Corn border; // Поле для назначения FieldBorder в инспекторе
     float borderPosX;
     public Animator animator;
     public float shootInterval = 1f;
     public float shootTimer = 0f;
- 
+
     private void Start()
     {
-        borderPosX = Corn.singleton.transform.position.x;
+        // Если border назначен в инспекторе, используем его, иначе берем singleton
+        borderPosX = (border != null ? border : Corn.singleton).transform.position.x;
     }
- 
+
     private void Update()
     {
         if (shootTimer > 0)
         {
             shootTimer -= Time.deltaTime;
         }
- 
+
         float enemyPosX = transform.position.x;
- 
+
         if (enemyPosX > borderPosX)
         {
             animator.SetBool("isMoving", true);
             transform.position += -transform.right * speed * Time.deltaTime;
- 
         }
         else
         {
             animator.SetBool("isMoving", false);
- 
+
             if (shootTimer <= 0)
             {
                 Attack();
@@ -42,18 +43,17 @@ public class Enemy : MonoBehaviour
             }
         }
     }
- 
+
     public void TakeDamage()
     {
         health -= 1;
- 
+
         if (health <= 0)
             Destroy(gameObject);
     }
- 
+
     public void Attack()
     {
         Corn.singleton.TakeDamage();
     }
- 
 }
